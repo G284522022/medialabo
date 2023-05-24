b7d8831773a322b6498db41f84f07d86058e0c22
G284522022 <105191809+G284522022@users.noreply.github.com>
2023-05-24 22:35:15 +0900

    あ

--- kazuate.js
+++ kazuate.js
@@ -8,26 +8,31 @@ let kaisu = 1;
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 hantei();
-hantei();
-hantei();
-hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  console.log(kaisu + '回目の予想：' + yoso)
+  let k = document.querySelector('span#kaisu');
+  k.textContent = kaisu;
+  let y = document.querySelector('span#answer');
+  y.textContent = yoso;
   if(kaisu < 5){
     if(yoso === kotae){
+      let p4 = document.querySelector('p#result');
+      p4.textContent = "正解です．おめでとう!";
       console.log("正解です．おめでとう!");
     }else if(yoso < kotae){
-      console.log("まちがい．答えはもっと大きいですよ");
+      let p4 = document.querySelector('p#result');
+      p4.textContent = "まちがい、答えはもっと大さいですよ";
     }else{
-      console.log("まちがい．答えはもっと小さいですよ");
+      let p4 = document.querySelector('p#result');
+      p4.textContent = 'まちがい、答えはもっと小さいですよ';
     }
     kaisu = kaisu + 1;
   }else{
-      console.lot("答えは" + kotae + "でした．すでにゲームは終わっています");
+      let p4 = document.querySelector('p#result');
+      p4.textContent = "答えは" + kotae + "でした．すでにゲームは終わっています";
       kaisu = kaisu + 1;
   }
   // 課題3-1: 正解判定する
