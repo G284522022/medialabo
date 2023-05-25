15a771fc5a1ed04c7f8d7f41ad03a4acc073035c
G284522022 <105191809+G284522022@users.noreply.github.com>
2023-05-25 17:06:09 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -1,21 +1,15 @@
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
-let w = document.querySelector('p');			// li#whale 要素を検索
+let w = document.querySelector('p');		
 w.remove();
-let p = document.createElement('button');				// 新しい p要素を作成
-p.textContent = '七面鳥';
 // 入力回数（予想回数）
+let b = document.querySelector('#kaitou');
+b.addEventListener('click', hantei);
 let kaisu = 1;
 
-// 予想を4回実行する
-// 将来以下の hantei(); の4回の呼び出しを全て削除する
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-
-// ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+	let i = document.querySelector('input[kazu="seisu"]');
+	let yoso = i.value;
   let k = document.querySelector('span#kaisu');
   k.textContent = kaisu;
   let y = document.querySelector('span#answer');
@@ -38,8 +32,15 @@ function hantei() {
       p4.textContent = "答えは" + kotae + "でした．すでにゲームは終わっています";
       kaisu = kaisu + 1;
   }
+}
+
+// 予想を4回実行する
+// 将来以下の hantei(); の4回の呼び出しを全て削除する
+// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+
+// ボタンを押した後の処理をする関数 hantei() の定義
+//function hante() {
+  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-
-}
